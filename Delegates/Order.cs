a112d53afda71fc9e268cb8ba3ae9a1791b0b797
ElshanAli <elshanml@code.edu.az>
2022-05-18 18:15:59 +0400

    Added Product Task

---
+++ Delegates/Order.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{
+    class Order
+    {
+        public double Totalprice;
+        public DateTime Date;
+
+        public Order()
+        {
+            Date = DateTime.Now;
+        }
+        public void Sale(Product product, int count)
+        {
+
+            double a = product.Price * count;
+            Totalprice += a;
+            Console.WriteLine($" The Cart price is { Math.Round(Totalprice, 2)} AZN");
+        }
+
+        public void Discount()
+        {
+            Predicate<double> nodiscount = Totalprice => Totalprice <= 100;
+            Predicate<double> discount10 = Totalprice => Totalprice > 100 && Totalprice < 200;
+            Predicate<double> discount20 = Totalprice => Totalprice >= 200;
+            if (nodiscount(Totalprice))
+            {
+                Console.WriteLine($" There is no discount -  {Math.Round(Totalprice, 2)} \n Thanks for shopping");
+            }
+            if (discount10(Totalprice))
+            {
+                Totalprice = Totalprice - Totalprice * 10/100;
+                Console.WriteLine($" 10 % discount -  {Math.Round(Totalprice, 2)} \n Thanks for shopping");
+            }
+            if (discount20(Totalprice))
+            {
+                Totalprice = Totalprice - Totalprice * 10/100;
+                Console.WriteLine($" 20 % discount -  {Math.Round(Totalprice, 2)} \n Thanks for shopping");
+            }
+            Console.WriteLine( Date);
+
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product("Chivas 12 years old ", 80, 50);
+            Product product2 = new Product("Chivas 18 years old ", 160, 40);
+            Product product3 = new Product("Black Label ", 120,70);
+            Console.WriteLine("Welcome to Market");
+           
+            Order order = new Order();
+            int selection;
+            int selection2;
+            do
+            {
+                Console.WriteLine("Please choose one of them");
+                Console.WriteLine("1 - Chivas 12 years old 80 AZN");
+                Console.WriteLine("2 - Chivas 18 years old 160 AZN");
+                Console.WriteLine("3 - Black Label 120 AZN");
+                Console.WriteLine("4 - Please indicate the Total Price");
+                selection = int.Parse(Console.ReadLine());
+
+                switch (selection)
+                {
+                    case 1:
+                        Console.WriteLine(" Choose how much you want to buy:");
+                        selection2 = int.Parse(Console.ReadLine());
+                        order.Sale(product1, selection2);
+                        break;
+                    case 2:
+                        Console.WriteLine(" Choose how much you want to buy:");
+                        selection2 = int.Parse(Console.ReadLine());
+                        order.Sale(product2, selection2);
+                        break;
+                    case 3:
+                        Console.WriteLine(" Choose how much you want to buy:");
+                        selection2 = int.Parse(Console.ReadLine());
+                        order.Sale(product3, selection2);
+                        break;
+                    case 4:
+                        order.Discount();
+                        
+
+
+
+                        break;
+
+
+                    default:
+                        break;
+                }
+
+
+            } while (selection > 0 && selection < 4);
+
+
+
+
+
+
+
+        
+    }
+        }
+}
